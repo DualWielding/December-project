af676cc5a8921c0e55859e98ab4f236531a39c0d
Maël ARNAUD <mael.arnaud@gmail.com>
2017-12-08 18:05:36 +0100

    Initial commit

---
+++ Character.gd
@@ -0,0 +1,64 @@
+extends Node2D
+
+const CHAR_SIZE = 32
+const TILE_SIZE = 32
+const GRAVITY = 1000
+const WALK_SPEED = 300
+const MAX_JUMP_SPEED = 600
+const JUMP_SPEED = 125
+const MIN_ONAIR_TIME = 0.1
+
+var velocity = Vector2()
+var effective_gravity = GRAVITY
+var on_floor = false
+
+func _ready():
+	set_fixed_process( true )
+
+func _fixed_process( delta ):
+	
+	if Input.is_action_pressed( "ui_up" ) and  on_floor and velocity.y > -MAX_JUMP_SPEED:
+		velocity.y -= JUMP_SPEED
+	else:
+		on_floor = false
+	
+	velocity.y += delta * effective_gravity
+	
+	if( Input.is_action_pressed( "ui_left" ) ):
+		velocity.x = -WALK_SPEED
+	elif( Input.is_action_pressed( "ui_right" ) ):
+		velocity.x = WALK_SPEED
+	else:
+		velocity.x = 0
+	
+	var motion = velocity * delta
+	move( motion )
+	
+	if( is_colliding() ):
+#		print(get_collider().get_pos().x, ", ", get_pos().x)
+#		if round(get_collider().get_pos().y) ==  round(get_pos().y + CHAR_SIZE):
+#			# Bot
+#			print("bot")
+#			on_floor = true
+#		if round(get_collider().get_pos().y) ==  round(get_pos().y - CHAR_SIZE):
+#			# Top
+#			print("top")
+#			pass
+#		if round(get_collider().get_pos().x) == round(get_pos().x + TILE_SIZE):
+#			# Right
+#			print("right")
+#			pass
+#		if round(get_collider().get_pos().x) == round(get_pos().x - TILE_SIZE):
+#			# Left
+#			print("left")
+#			pass
+#		
+#		print("------------")
+		
+		var n = get_collision_normal()
+		motion = n.slide( motion )
+		velocity = n.slide( velocity )
+		move( motion )
+		
+		if n == Vector2(0, -1):
+				on_floor = true
