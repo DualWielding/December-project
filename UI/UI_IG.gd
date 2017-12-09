fa1dd846de65969565156108d339d11cc0cd4ffa
Maël ARNAUD <mael.arnaud@gmail.com>
2017-12-09 20:54:34 +0100

    Death keep coins updated

--- UI/UI_IG.gd
+++ UI/UI_IG.gd
@@ -4,6 +4,7 @@ onready var coins_number = get_node( "CoinsNumber" )
 
 func _ready():
 	Player.connect( "coins_updated", self, "update_coins" )
+	update_coins( Player.coins )
 
 func update_coins( current_amount ):
 	coins_number.set_text( str( "Coins: ", current_amount ) )
