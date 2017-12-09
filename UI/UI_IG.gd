e373ac455d3ce64022e2cf6e04f3a4d59e2410a1
Maël ARNAUD <mael.arnaud@gmail.com>
2017-12-09 18:43:42 +0100

    Added UI

---
+++ UI/UI_IG.gd
@@ -0,0 +1,9 @@
+extends CanvasLayer
+
+onready var coins_number = get_node( "CoinsNumber" )
+
+func _ready():
+	Player.connect( "coins_updated", self, "update_coins" )
+
+func update_coins( current_amount ):
+	coins_number.set_text( str( "Coins: ", current_amount ) )
