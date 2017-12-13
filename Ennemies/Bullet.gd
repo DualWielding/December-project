ef0799d292aae96aef8893f1f4de139c1a0dfa4f
Maël ARNAUD <mael.arnaud@gmail.com>
2017-12-13 17:47:23 +0100

    Minor fixes

--- Ennemies/Bullet.gd
+++ Ennemies/Bullet.gd
@@ -1,4 +1,7 @@
 extends "res://Ennemies/Ennemy.gd"
 
+func _ready():
+	add_to_group( "bullet" )
+
 func _on_VisibilityNotifier2D_exit_screen():
 	queue_free()
--- Ennemies/ShootingEnnemy.gd
+++ Ennemies/ShootingEnnemy.gd
@@ -11,6 +11,12 @@ export( int ) var RELOAD_TIME = 1
 var _reloaded = true
 
 func _ready():
+	if STARTING_DIRECTION == "Left":
+		current_direction = DIRECTION_LEFT
+	else:
+		current_direction = DIRECTION_RIGHT
+	_set_ray()
+	
 	at.set_wait_time( RELOAD_TIME )
 	at.connect( "timeout", self, "reload" )
 	at.set_one_shot( true )
@@ -25,15 +31,14 @@ func _fixed_process(delta):
 func shoot():
 	if _reloaded:
 		var b = bullet_scene.instance()
-		b.add_to_group( "bullet" )
-		b.current_direction = current_direction
 		
 		var pos = get_pos()
 		
 		if current_direction == DIRECTION_RIGHT:
 			pos.x += character_size.x
 		
-		Player.current_level.add_enemy( b, get_pos() )
+		Player.current_level.add_enemy( b, pos )
+		b.current_direction = current_direction
 		at.start()
 		_reloaded = false
 
